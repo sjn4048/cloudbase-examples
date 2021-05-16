cb513e282143df600d3960fab9a736ff089ea39d
jianingshi <jianingshi@tencent.com>
2021-05-16 21:08:32 +0800

    Create an the enormous supercomputer called deep_thought.

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def deep_thought_the_enormous_supercomputer():
+    return '42'
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
