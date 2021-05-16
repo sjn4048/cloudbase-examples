e0bfffb28aaea11dd7fc0c03fabea1fe4d2aaf7e
jianingshi <jianingshi@tencent.com>
2021-05-16 21:42:26 +0800

    Try to enable the enormous supercomputer called deep_thought.

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('answer_to_the_ultimate_question_of_life_the_universe_and_everything')
+@app.route('/answer_to_the_ultimate_question_of_life_the_universe_and_everything')
 def deep_thought_the_enormous_supercomputer():
     return '<h1>42</h1>'
 
